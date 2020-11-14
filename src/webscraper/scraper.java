1282445c30fd2752ac93e5139a795fe1cc480fc2
Jonas <jonaslpaul2303@gmail.com>
2020-11-14 16:07:37 +0100

    PP-14 | refactors scraper.java

--- src/webscraper/scraper.java
+++ src/webscraper/scraper.java
@@ -3,25 +3,29 @@ package webscraper;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-//import webscraper.LinkBuilder;
 
 public class scraper {
 
 	public static void main(String[] args) {
-		
+		System.out.println(getAmountOfHits());
+	}
+	
+	public static String getAmountOfHits() {
 		LinkBuilder linkBuilder = new LinkBuilder();
 		
 		String url = linkBuilder.buildLink("natural AND disaster");
+		String hits = "";
 		
 		try{
 			final Document document = Jsoup.connect(url).get();
 		
-			String test = document.select("div.gs_ab_mdw").text();
-			System.out.println(test); 
+			hits = document.select("div.gs_ab_mdw").text();
 		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
+		return hits;
 	}
 }
 
