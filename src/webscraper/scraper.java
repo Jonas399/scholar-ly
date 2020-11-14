b94ad82a9cfa5dca8286a9d92cd111a7b8827df2
Jonas <jonaslpaul2303@gmail.com>
2020-11-14 16:25:24 +0100

    PP-33 | result now only includes hit count

--- src/webscraper/scraper.java
+++ src/webscraper/scraper.java
@@ -7,19 +7,20 @@ import org.jsoup.nodes.Element;
 public class scraper {
 
 	public static void main(String[] args) {
-		System.out.println(getAmountOfHits());
+		System.out.println(getAmountOfHits("natural AND disaster"));
 	}
 	
-	public static String getAmountOfHits() {
+	public static String getAmountOfHits(String searchTerm) {
 		LinkBuilder linkBuilder = new LinkBuilder();
 		
-		String url = linkBuilder.buildLink("natural AND disaster");
+		String url = linkBuilder.buildLink(searchTerm);
 		String hits = "";
 		
 		try{
 			final Document document = Jsoup.connect(url).get();
 		
 			hits = document.select("div.gs_ab_mdw").text();
+			hits = hits.substring(25, hits.length()-23);
 		
 		} catch(Exception e) {
 			e.printStackTrace();
