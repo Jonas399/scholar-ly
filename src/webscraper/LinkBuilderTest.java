4aaf7b2b42c25e7d85a352e893f8d6e8baa8d95c
Jonas <jonaslpaul2303@gmail.com>
2020-11-14 14:23:44 +0100

    PP-30 | Creates JUnit test for LinkBuilder

---
+++ src/webscraper/LinkBuilderTest.java
@@ -0,0 +1,16 @@
+package webscraper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LinkBuilderTest {
+
+	LinkBuilder linkBuilderTest = new LinkBuilder();
+	
+	@Test
+	void testIfLinkGetsCreatedCorrectly() {
+		assertEquals("https://scholar.google.de/scholar?hl=de&as_sdt=0%2C5&q=natural+AND+disaster&btnG=", linkBuilderTest.buildLink("natural AND disaster"));
+	}
+
+}
