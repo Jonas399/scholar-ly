1bd9d00c7baf28c079366f4c0b5212875786f659
Jonas <jonaslpaul2303@gmail.com>
2020-11-14 14:06:10 +0100

    PP-17 | implements feature to search for multiple terms in a row

--- src/webscraper/LinkBuilder.java
+++ src/webscraper/LinkBuilder.java
@@ -3,8 +3,13 @@ package webscraper;
 
 public class LinkBuilder {
 	public String buildLink(String searchTerm) {
+		//beginString and endString are consistent for standard search
+		//TODO: Add possibility to add time aspect to search --> other string format
 		String beginString =  "https://scholar.google.de/scholar?hl=de&as_sdt=0%2C5&q=";
 	    String endString =	"&btnG=";
+	    
+	    //replace "" with "+" to ensure right format for link --> multiple terms can be used
+	    searchTerm = searchTerm.replace("", "+");
 	    
 	    String url = beginString + searchTerm + endString;
 	    
