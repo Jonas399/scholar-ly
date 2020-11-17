acdaddc56e2af8a2109aaaafac7c1ff831168972
Jonas <jonaslpaul2303@gmail.com>
2020-11-17 18:02:38 +0100

    PP-30 | adds further tests of linkbuilder

--- src/test/java/com/scholar/projektseminar_programmierung/LinkBuilderTest.java
+++ src/test/java/com/scholar/projektseminar_programmierung/LinkBuilderTest.java
@@ -8,6 +8,7 @@ class LinkBuilderTest {
 
 	LinkBuilder linkBuilderTest = new LinkBuilder();
 	
+	//Tests check weather the link gets builded correctly
 	@Test
 	void testIfLinkGetsCreatedCorrectly_noTimeFrame() {
 		assertEquals("https://scholar.google.de/scholar?hl=de&as_sdt=0%2C5&q=natural+AND+disaster&hl=de&as_sdt=0%2C5&as_ylo=&as_yhi=", linkBuilderTest.buildLink("natural AND disaster",0,0));
@@ -26,5 +27,21 @@ class LinkBuilderTest {
 	@Test
 	void testIfLinkGetsCreatedCorrectly_yearBeginAndyearEnd() {
 		assertEquals("https://scholar.google.de/scholar?hl=de&as_sdt=0%2C5&q=natural+AND+disaster&hl=de&as_sdt=0%2C5&as_ylo=2015&as_yhi=2020", linkBuilderTest.buildLink("natural AND disaster", 2015, 2020));
+	}
+	
+	//Tests check result with negative parameters
+	@Test
+	void testIfLinkGetsCreatedCorrectly_yearBeginIsNegative() {
+		assertEquals("https://scholar.google.de/scholar?hl=de&as_sdt=0%2C5&q=natural+AND+disaster&hl=de&as_sdt=0%2C5&as_ylo=&as_yhi=", linkBuilderTest.buildLink("natural AND disaster", -2010, 0));
+	}
+	
+	@Test
+	void testIfLinkGetsCreatedCorrectly_yearEndIsNegative() {
+		assertEquals("https://scholar.google.de/scholar?hl=de&as_sdt=0%2C5&q=natural+AND+disaster&hl=de&as_sdt=0%2C5&as_ylo=&as_yhi=", linkBuilderTest.buildLink("natural AND disaster", 0, -2010));
+	}
+	
+	@Test
+	void testIfLinkGetsCreatedCorrectly_yearBeginAndYearEndAreNegative() {
+		assertEquals("https://scholar.google.de/scholar?hl=de&as_sdt=0%2C5&q=natural+AND+disaster&hl=de&as_sdt=0%2C5&as_ylo=&as_yhi=", linkBuilderTest.buildLink("natural AND disaster", -2015, -2020));
 	}
 }
