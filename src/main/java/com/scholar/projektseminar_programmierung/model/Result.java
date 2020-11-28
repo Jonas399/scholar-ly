a1a65ab2749a41a70894bd86bbadb4a0ead9feea
Jakob Spahn <jakob@craalse.de>
2020-11-28 12:08:22 +0100

    PP-19 | CSV full functionality

--- src/main/java/com/scholar/projektseminar_programmierung/model/Result.java
+++ src/main/java/com/scholar/projektseminar_programmierung/model/Result.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.scholar.projektseminar_programmierung.Scraper;
+
 import com.scholar.projektseminar_programmierung.Search;
 
 
