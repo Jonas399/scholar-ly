1bd70ea6740a27507c8b39128493bc051888c350
Jakob Spahn <jakob@craalse.de>
2021-01-17 10:59:47 +0100

    cleanup

--- src/main/java/com/scholar/projektseminar_programmierung/ProjektseminarProgrammierungApplication.java
+++ src/main/java/com/scholar/projektseminar_programmierung/ProjektseminarProgrammierungApplication.java
@@ -6,33 +6,42 @@ import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class ProjektseminarProgrammierungApplication {
+	
+	public static void startBrowser(String url, String os) {
+		try {
+			Runtime runtime = Runtime.getRuntime();
+			if(os.contains("windows")) {
+				runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+			}
+			else if(os.contains("mac")) {
+				runtime.exec("open "+url);
+			}
+			else if(os.contains("nix") || os.contains("nux")) {
+				runtime.exec("xdg-open "+url);
+			} else {
+				System.out.println("Unable to launch browser");
+			}
+		} catch(IOException eek) {
+			System.out.println("Error: "+ eek.getMessage());
+	}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjektseminarProgrammierungApplication.class, args);
 		
 		//Open Browser
 		String url = "http://localhost:8080";
-		
 		String myOS = System.getProperty("os.name").toLowerCase();
+		startBrowser(url,myOS);
 		
-		try {
-				Runtime runtime = Runtime.getRuntime();
-				if(myOS.contains("windows")) {
-					runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
-				}
-					else if(myOS.contains("mac")) {
-					runtime.exec("open "+url);
-				}
-				else if(myOS.contains("nix") || myOS.contains("nux")) {
-					runtime.exec("xdg-open "+url);
-				} else {
-					System.out.println("Unable to launch browser");
-				}
-			} catch(IOException eek) {
-				System.out.println("Error: "+eek.getMessage());
-		}
+		
+
 	}
 
 }
+
